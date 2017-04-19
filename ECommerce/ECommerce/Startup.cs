dc468bcab642465fa2c7598b627dea6214411f55
Igor Ramos De Luna <igor.lunarx@gmail.com>
2017-04-19 17:04:19 -0300

    Adicionando projeto web e controllers para categoria e produto

--- ECommerce/ECommerce/Startup.cs
+++ ECommerce/ECommerce/Startup.cs
@@ -58,6 +58,10 @@ namespace ECommerce
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
+                    name: "AdminAreaRoute", 
+                    template: "{area:exists}/{controller=Products}/{action=Index}/{id?}");
             });
 
             
